ce26f4464ce1d1fd919add5c62cb8922796a33e3
lenowac <lenowac@pm.me>
2022-05-23 21:17:59 +0200

    Update install-choco.bat

--- src/install-choco.bat
+++ src/install-choco.bat
@@ -2,10 +2,7 @@
 :: works standalone
 @echo off
 echo this will run the choco installer.
-
-:: Privilige escalation
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+echo. 
 
 :: Install Choco
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
