193815a377a9715c9ad5ca56c73749b4c47d94cc
lenowac <lenowac@pm.me>
2022-05-23 21:18:16 +0200

    Update install-choco.bat

--- src/install-choco.bat
+++ src/install-choco.bat
@@ -1,8 +1,5 @@
 :: Batch Script
 :: works standalone
-@echo off
-echo this will run the choco installer.
-echo. 
 
 :: Install Choco
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
